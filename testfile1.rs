7dc279c0168d974bceff41ac814c830b5e17c1db
arnica-engineering <95713264+arnica-engineering@users.noreply.github.com>
2025-04-15 16:40:52 -0400

    Update testfile1.rs

--- testfile1.rs
+++ testfile1.rs
@@ -2,17 +2,17 @@ use reqwest::header;
 use reqwest::{blocking::Client, header::HeaderMap, header::HeaderValue, Url};
 
 // ruleid: reqwest-set-sensitive
-let mut headers = header::HeaderMap::new();
+
 let header = header::HeaderValue::from_static("secret");
 headers.insert(header::AUTHORIZATION, header);
 
 // ruleid: reqwest-set-sensitive
-let mut headers = header::HeaderMap::new();
+
 let header = header::HeaderValue::from_static("secret");
 headers.insert("Authorization", header);
 
 // ruleid: reqwest-set-sensitive
-let mut headers = header::HeaderMap::new();
+
 let header = header::HeaderValue::from_static("secret").map_err(|e| {
     Error::Generic(format!(
         "Error"
